87fb5cebebfcb9a97e0a0b15b46e7aa7fe202de8
BEREKET MUSSIE <bereketmuse42@gmail.com>
2026-05-06 00:43:03 +0300

    Update Mapping.sol

--- Learn solidity/member4_Bereket/Mapping/Mapping.sol
+++ Learn solidity/member4_Bereket/Mapping/Mapping.sol
@@ -105,27 +105,3 @@ contract Contract {
         connections[msg.sender][other] = connectionType;
     }
 }
-
-Voting 1
-
-
-contract Voting {
-    struct Proposal {
-        address target;
-        bytes data;
-        uint yesCount;
-        uint noCount;
-    }
-
-    Proposal[] public proposals;
-
-    function newProposal(address _target, bytes calldata _data) external {
-        proposals.push(Proposal({
-            target: _target,
-            data: _data,
-            yesCount: 0,
-            noCount: 0
-        }));
-    }
-}
-
