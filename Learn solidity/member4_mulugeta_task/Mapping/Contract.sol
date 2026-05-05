cd51704b4bdf5fd109a4e8dd9b9a9c92c7e7a195
Muller16 <mulugetaabeje16@gmail.com>
2026-05-06 01:27:47 +0300

    Add multiple contract mappings with user management and connection types

---
+++ Learn solidity/member4_mulugeta_task/Mapping/Contract.sol
@@ -0,0 +1,107 @@
+Mapping 1
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address _addr) external {
+        members[_addr] = true;
+    }
+}
+
+
+Mapping 2
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address _addr) external {
+        members[_addr] = true;
+    }
+
+    function isMember(address _addr) external view returns (bool) {
+        return members[_addr];
+    }
+}
+
+Mapping 3
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address _addr) external {
+        members[_addr] = true;
+    }
+
+    function isMember(address _addr) external view returns (bool) {
+        return members[_addr];
+    }
+
+    function removeMember(address _addr) external {
+        members[_addr] = false;
+    }
+}
+
+Mapping 4
+
+contract Contract {
+    struct User {
+        uint balance;
+        bool isActive;
+    }
+
+    mapping(address => User) public users;
+
+    function createUser() external {
+        require(!users[msg.sender].isActive, "User already exists");
+
+        users[msg.sender] = User({
+            balance: 100,
+            isActive: true
+        });
+    }
+}
+
+Mapping 5
+
+contract Contract {
+    struct User {
+        uint balance;
+        bool isActive;
+    }
+
+    mapping(address => User) public users;
+
+    function createUser() external {
+        require(!users[msg.sender].isActive, "User already exists");
+        users[msg.sender] = User({
+            balance: 100,
+            isActive: true
+        });
+    }
+
+    function transfer(address _recipient, uint _amount) external {
+        require(users[msg.sender].isActive, "Sender must be active");
+        require(users[_recipient].isActive, "Recipient must be active");
+        require(users[msg.sender].balance >= _amount, "Insufficient balance");
+
+        users[msg.sender].balance -= _amount;
+        users[_recipient].balance += _amount;
+    }
+}
+
+Mapping 6
+
+contract Contract {
+    enum ConnectionTypes { 
+        Unacquainted, 
+        Friend, 
+        Family, 
+        Spouse 
+    }
+
+    mapping(address => mapping(address => ConnectionTypes)) public connections;
+
+    function connectWith(address other, ConnectionTypes connectionType) external {
+        connections[msg.sender][other] = connectionType;
+    }
+}
